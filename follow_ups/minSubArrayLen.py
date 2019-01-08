0d05503c031a675c11d93aecafd17d49e8e9f0bf
RachelChen <lei.chen2@duke.edu>
2019-01-08 16:26:26 -0500

    Create minSubArrayLen.py

---
+++ follow_ups/minSubArrayLen.py
@@ -0,0 +1,15 @@
+# result init: 
+  # has to be > len(nums), for the case no answer is found 
+
+class Solution:
+
+def minSubArrayLen(self, s, nums):
+    total = left = 0
+    result = len(nums) + 1
+    for right, n in enumerate(nums):
+        total += n
+        while total >= s:
+            result = min(result, right - left + 1)
+            total -= nums[left]
+            left += 1
+    return result if result <= len(nums) else 0
