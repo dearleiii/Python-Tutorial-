61ded945419343701dedc99f461f3833aa785b6e
RachelChen <lei.chen2@duke.edu>
2019-01-08 16:43:07 -0500

    Create lengthOfLongestSubstring.py

---
+++ follow_ups/lengthOfLongestSubstring.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param s: a string
+    @return: an integer
+    """
+    def lengthOfLongestSubstring(self, s):
+        # write your code here
+        start = maxLength = 0
+        usedChar = {}
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+                
+            usedChar[s[i]] = i
+        
+        return maxLength          
