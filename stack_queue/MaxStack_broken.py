f57705d26404738ba3a5f18cf8f7ba3be1815968
RachelChen <lei.chen2@duke.edu>
2019-02-02 11:52:13 -0500

    Create MaxStack_broken.py

---
+++ stack_queue/MaxStack_broken.py
@@ -0,0 +1,60 @@
+class MaxStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.maxstack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+        if len(self.maxstack) == 0 or self.maxstack[-1][0] <= x: 
+            self.maxstack.append((x, len(self.maxstack)))
+        else: 
+            self.maxstack.append((self.maxstack[-1][0], len(self.maxstack)))
+            
+        #print(self.stack)
+        #print(self.maxstack)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        number = self.stack.pop()
+        self.maxstack.pop()
+        return number
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def peekMax(self):
+        """
+        :rtype: int
+        """
+        return self.maxstack[-1][0]
+
+    def popMax(self):
+        """
+        :rtype: int
+        """
+        max_num, index = self.maxstack[-1]
+        self.maxstack.pop()
+        self.stack.pop(index)
+        return max_num
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
